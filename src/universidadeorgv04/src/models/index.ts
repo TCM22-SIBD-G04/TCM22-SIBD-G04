652d475951e74ed6de16bc5dbb2bc33a65b082be
JoseMSoares <josesoares727@gmail.com>
2023-06-20 18:55:54 +0100

    New api 20-06

--- src/universidadeorgv04/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './professores.model';
-export * from './departamento.model';
-export * from './estudantes.model';
-export * from './organismo-financiador.model';
-export * from './projetos.model';
--- src/universidadeorgv04/src/repositories/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './departamento.repository';
-export * from './estudantes.repository';
-export * from './organismo-financiador.repository';
-export * from './professores.repository';
-export * from './projetos.repository';
